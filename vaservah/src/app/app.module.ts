0e526ee8cb966cc13ad2eaff9c2f19175a6cdb30
swagoverlord <azcooldude8@gmail.com>
2023-02-20 14:05:01 +0100

    Update app.module.ts

--- vaservah/src/app/app.module.ts
+++ vaservah/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
