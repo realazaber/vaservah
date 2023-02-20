c760fecee0457f1fef5d0e7c8491b3cca6b632a6
swagoverlord <azcooldude8@gmail.com>
2023-02-20 13:51:00 +0100

    Created base pages and tested ng universal

--- vaservah/src/app/app-routing.module.ts
+++ vaservah/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'posts', loadChildren: () => import('./pages/posts/posts.module').then(m => m.PostsModule) },
+  { path: 'contact', loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) },
+  { path: 'error', loadChildren: () => import('./pages/error/error.module').then(m => m.ErrorModule) },
+  { path: '**', redirectTo: 'error', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
