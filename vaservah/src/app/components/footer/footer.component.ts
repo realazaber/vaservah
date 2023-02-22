c6ce416672270ea3d3014ad394ee68ad6e6dbceb
swagoverlord <azcooldude8@gmail.com>
2023-02-22 01:39:32 +0100

    URL links appear in nav and footer

--- vaservah/src/app/components/footer/footer.component.ts
+++ vaservah/src/app/components/footer/footer.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetUrlsService } from 'src/app/services/get-urls.service';
+import { Link } from 'src/app/interfaces/Link';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+
+  constructor(private urls: GetUrlsService) { }
+
+  links: Link[] = [];
+
+  ngOnInit(): void {
+    this.urls.getUrls().subscribe((data) => this.links = data);
+  }
 
 }
