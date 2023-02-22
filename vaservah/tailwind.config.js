5c7830a6e81ecf56de5a5e6547e6df89d193e3df
swagoverlord <azcooldude8@gmail.com>
2023-02-22 02:12:24 +0100

    Default tailwind config

--- vaservah/tailwind.config.js
+++ vaservah/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/pages/*.{html,ts}",
-    "./src/app/components/*.{html,ts}",
     "./src/**/*.{html,ts}",
   ],
   theme: {
