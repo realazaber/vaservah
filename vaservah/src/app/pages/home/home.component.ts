911102c56c106509a22c910248e960521e92d705
swagoverlord <azcooldude8@gmail.com>
2023-02-22 12:45:05 +0100

    Home page data is gathered from wordpress

--- vaservah/src/app/pages/home/home.component.ts
+++ vaservah/src/app/pages/home/home.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { GetPagesService } from 'src/app/services/get-pages.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  homePgraph: String = "Home content";
+  homeBannerImg: String = "pathtobanner";
+  homeBannerImgAlt: String = "Alt text";
 
+  constructor(private pagesService: GetPagesService) {
+
+  }
+
+  ngOnInit(): void {
+    this.pagesService.getPageData('24').subscribe((data) => {
+      this.homePgraph = data.content.rendered;
+      this.homeBannerImgAlt = data.better_featured_image.alt_text;
+      this.homeBannerImg = data.better_featured_image.media_details.sizes.large.source_url;
+    })
+  }
+  
 }
