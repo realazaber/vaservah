bfe6fbf933e18adb1a4cde3dc0fe521b504f864d
swagoverlord <azcooldude8@gmail.com>
2023-02-22 12:41:00 +0100

    About page gets wordpress data

--- vaservah/src/app/pages/about/about.component.ts
+++ vaservah/src/app/pages/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { GetPagesService } from 'src/app/services/get-pages.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  aboutPgraph: String = "About content";
+  aboutBannerImg: String = "pathtobanner";
+  aboutBannerImgAlt: String = "Alt text";
+
+  constructor(private pagesService: GetPagesService) {
+
+  }
+
+  ngOnInit(): void {
+    this.pagesService.getPageData('26').subscribe((data) => {
+      this.aboutPgraph = data.content.rendered;
+      this.aboutBannerImgAlt = data.better_featured_image.alt_text;
+      this.aboutBannerImg = data.better_featured_image.media_details.sizes.large.source_url;
+    })
+  }
 }
