a80a03f0d51248c053337a653be6ea599598f02c
swagoverlord <azcooldude8@gmail.com>
2023-02-23 04:20:32 +0100

    Removed unused module from app.module

--- vaservah/src/app/app.module.ts
+++ vaservah/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavComponent } from './components/nav/nav.component';
-import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -19,6 +18,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     HttpClientModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
